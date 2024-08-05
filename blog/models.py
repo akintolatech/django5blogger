87cfbd6d010e1866a9c662c123e46ffe9bcb4b8f
akintolatech <akintolatechnologies@gmail.com>
2024-08-05 18:08:58 +0100

    Initial commit

---
+++ blog/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+from django.utils import timezone
+from django.db.models.functions import Now
+from django.conf import settings
+
+
+# Create your models here.
+class Post(models.Model):
+    class Status(models.TextChoices):
+        DRAFT = "DF", "Draft"
+        PUBLISHED = "PB", "Published"
+
+    title = models.CharField(max_length=33)
+    slug = models.SlugField(max_length=250)
+    body = models.TextField()
+
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="blog_posts"
+
+    )
+
+    # publish = models.DateTimeField(default=timezone.now)
+    publish = models.DateTimeField(db_default=Now())  #database generated values
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    status = models.CharField(
+        max_length=2,
+        choices=Status,
+        default=Status.DRAFT
+    )
+
+    class Meta:
+        ordering = ["-publish"]
+        indexes = [
+            models.Index(fields=["-publish"])
+        ]
+
+    def __str__(self):
+        return self.title
