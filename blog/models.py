08430f7990ae5c2c8fbca781158d9553b1bc1d65
akintolatech <akintolatechnologies@gmail.com>
2024-08-20 23:59:19 +0100

    custon manager

--- blog/models.py
+++ blog/models.py
@@ -8,6 +8,7 @@ from django.conf import settings
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
+
         return (
         super().get_queryset().filter(status=Post.Status.PUBLISHED)
     )
